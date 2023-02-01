977fbec074c01c187a567710d58979d834d69418
cingfong <44254681+cingfong@users.noreply.github.com>
2023-02-01 14:10:59 +0800

    fix: deal cards & compare

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// 新增原生陣列方法
+Array.prototype.remove = function (val) {
+    this.splice(this.indexOf(val), 1)
+}
 const cardsNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-const cardsType = ['club', 'heart', 'squre', 'daimond']
+const cardsType = ['club', 'heart', 'square', 'diamond']
 const pokerList = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]]
 class User {
     constructor(money, handCards) {
@@ -18,6 +22,7 @@ function randomCard() {
     const number = getRandomNum(13) + 1
     const checkCard = pokerList[type].includes(number)
     if (!checkCard) return randomCard()
+    pokerList[type].remove(number)
     return { suits: cardsType[type], number: number }
 }
 function dealCard(...userList) {
@@ -26,5 +31,60 @@ function dealCard(...userList) {
         item.addCard(card)
     })
 }
-dealCard(user1, bookmaker)
-console.log(user1, bookmaker)
+
+const startBtn = document.getElementById('start')
+const addBtn = document.getElementById('add')
+const compareBtn = document.getElementById('compare')
+const bookmakerMoney = document.getElementById('bookmakerMoney')
+const user1Money = document.getElementById('user1Money')
+const bookmakerHandCards = document.getElementById('bookmakerHandCards')
+const user1HandCards = document.getElementById('user1HandCards')
+bookmakerMoney.textContent = bookmaker.money
+user1Money.textContent = user1.money
+startBtn.addEventListener('click', () => {
+    dealCard(bookmaker, user1)
+    user1HandCards.textContent = user1.handCards[0].number > 10 ? 0.5 : user1.handCards[0].number
+    bookmakerHandCards.textContent = bookmaker.handCards[0].number > 10 ? 0.5 : bookmaker.handCards[0].number
+})
+addBtn.addEventListener('click', () => {
+    dealCard(user1)
+    user1HandCards.textContent = user1.handCards.reduce((total, item) => {
+        return Number(item.number > 10 ? 0.5 : item.number) + total
+    }, 0)
+    if (user1HandCards.textContent > 10.5) {
+        setTimeout(() => { alert('爆炸') }, 0)
+        return
+    }
+    if (user1.handCards.length > 4) {
+        setTimeout(() => { alert('過五關') })
+    }
+})
+compareBtn.addEventListener('click', () => {
+    compareAllHandCards()
+})
+function compareAllHandCards() {
+    const user1HandTotal = user1.handCards.reduce((total, item) => {
+        return Number(item.number > 10 ? 0.5 : item.number) + total
+    }, 0)
+    const bookmakerHandTotal = bookmaker.handCards.reduce((total, item) => {
+        return Number(item.number > 10 ? 0.5 : item.number) + total
+    }, 0)
+    bookmakerHandCards.textContent = bookmakerHandTotal
+    if (bookmakerHandTotal > 10.5) {
+        console.log(bookmaker.handCards)
+        setTimeout(() => { alert('莊家爆炸') }, 0)
+        return
+    }
+    if (bookmakerHandTotal > user1HandTotal) {
+        setTimeout(() => { alert('莊家點數大') }, 0)
+        return
+    }
+    if (bookmakerHandTotal === user1HandTotal) {
+        setTimeout(() => { alert('和局') }, 0)
+        return
+    }
+    if (bookmakerHandTotal < 8.5) {
+        dealCard(bookmaker)
+    }
+    compareAllHandCards()
+}
